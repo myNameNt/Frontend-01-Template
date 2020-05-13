d5625ac208626b1321e480ec50d5e796aebfa641
myNameNt <381836999@qq.com>
2020-05-13 23:30:14 +0800

    第五周作业

--- week05/client.js
+++ week05/client.js
@@ -1,32 +1,65 @@
 const net = require('net')
 
 class Request {
+    // method url = host + port + path
+    // body = k/v
+    // headers
+    constructor(options) {
+        this.method = options.method || 'GET'
+        this.host = options.host;
+        this.port = options.host || 80
+        this.path = options.path || '/'
+        this.body = options.body || {}
+        this.headers = options.headers || {}
+        if (!this.headers["Content-Type"]) {
+            this.headers["Content-Type"] = 'application/x-www-form-urlencoded'
+        }
 
+        if (this.headers["Content-Type"] === 'application/json') {
+            this.bodyText = JSON.stringify(this.body)
+        } else if (this.headers["Content-Type"] === 'application/x-www-form-urlencoded') {
+            this.bodyText = Object.keys(this.body).map(key => {
+                return `${key}=${encodeURIComponent(this.body[key])}`
+            }).join('&')
+        }
+        this.headers['Content-Length'] = this.bodyText.length
+    }
+    toString() {
+        return [
+            `${this.method} ${this.path} HTTP/1.1`,
+            `${Object.entries(this.headers).map(([k, v]) => `${k}: ${v}`).join('\r\n')}`,
+            '',
+            `${this.bodyText}`
+          ].join('\r\n');
+    }
+    open() {
+
+    }
+    send() { }
 }
 
 class Response {
 
 }
 
-// net.connect({
-//     address: 'location',
-//     port: '8888',
-//     onread: {
-//         buffer: Buffer.alloc(4*1024),
-//         callback: function (nread, buf) {
-//             console.log(buf.toString('utf8',0,nread))
-//         }
-//     }
-// })
-let client = net.createConnection({port: 8888,host: '127.0.0.1'}, ()=>{
-    console.log('connect to server')
-    client.write('world \r \n')
+let client = net.createConnection({ port: 8888, host: '127.0.0.1' }, () => {
+    let request = new Request({
+        method: 'GET',
+        host: '127.0.0.1',
+        port: '8888',
+        path: '/',
+        body: {
+            name: 'ha'
+        }
+    })
+    console.log(request.toString())
+    client.write(request.toString())
 })
-client.on('data', (data)=>{
+client.on('data', (data) => {
     console.log(data.toString())
     client.end()
 })
 
-client.on('end', function(){
+client.on('end', function () {
     console.log('disconnected from server')
 })
