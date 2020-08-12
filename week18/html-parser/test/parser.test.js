7a9d6f165429284ecb29411a505841546b382bdd
myNameNt <381836999@qq.com>
2020-08-12 22:52:20 +0800

    test case

--- week18/html-parser/test/parser.test.js
+++ week18/html-parser/test/parser.test.js
@@ -1,7 +1,157 @@
-import {parseHTML} from '../src/parser'
+let parseHTML = require('../src/parser.js')
+let assert = require('assert');
 
-let assert = require('assert')
+it('parse a single element', function () {
+  let document = parseHTML.parseHTML('<div></div>');
+  let div = document.children[0];
 
-it('parseHTML', ()=>{
-  parseHTML('<div></div>')
-})
+  assert.equal(div.tagName, 'div');
+  assert.equal(div.children.length, 0);
+  assert.equal(div.type, 'element');
+  assert.equal(div.attributes.length, 2);
+});
+
+it('parse a single element with text content', function () {
+  let document = parseHTML.parseHTML('<div>hello</div>');
+  let text = document.children[0].children[0];
+
+  assert.equal(text.content, 'hello');
+  assert.equal(text.type, 'text');
+});
+
+it('tag mismatch', function () {
+  try {
+    let document = parseHTML.parseHTML('<div></vid>');
+  } catch (e) {
+    assert.equal(e.message, "Tag start end doesn't match!");
+  }
+});
+
+it('test with <', function () {
+  let document = parseHTML.parseHTML('<div>a < b</div>');
+  let text = document.children[0].children[0];
+  assert.equal(text.content, 'a < b');
+  assert.equal(text.type, 'text');
+});
+
+/* it('with property', function () {
+  let document = parseHTML('<div id=a></div>');
+  let div = document.children[0];
+  for (const attr of div.attributes) {
+    if (attr.name === 'id') {
+      assert.equal(attr.value, 'a');
+      return;
+    }
+  }
+  assert.ok(false);
+}); */
+
+it('with property', function () {
+  let document = parseHTML.parseHTML(`<div id=a class='cls' data="abc" ></div>`);
+  let div = document.children[0];
+
+  let count = 0;
+
+  for (const attr of div.attributes) {
+    if (attr.name === 'id') {
+      count++;
+      assert.equal(attr.value, 'a');
+    }
+    if (attr.name === 'class') {
+      count++;
+      assert.equal(attr.value, 'cls');
+    }
+    if (attr.name === 'data') {
+      count++;
+      assert.equal(attr.value, 'abc');
+    }
+  }
+
+  assert.ok(count === 3);
+});
+
+it('with property2', function () {
+  let document = parseHTML.parseHTML(`<div id=a class='cls' data="abc"></div>`);
+  let div = document.children[0];
+
+  let count = 0;
+
+  for (const attr of div.attributes) {
+    if (attr.name === 'id') {
+      count++;
+      assert.equal(attr.value, 'a');
+    }
+    if (attr.name === 'class') {
+      count++;
+      assert.equal(attr.value, 'cls');
+    }
+    if (attr.name === 'data') {
+      count++;
+      assert.equal(attr.value, 'abc');
+    }
+  }
+
+  assert.ok(count === 3);
+});
+
+it('with property 3', function () {
+  let document = parseHTML.parseHTML('<div id=a class="cls"data="abc"/>');
+  let div = document.children[0];
+
+  let count = 0;
+
+  for (const attr of div.attributes) {
+    if (attr.name === 'id') {
+      count++;
+      assert.equal(attr.value, 'a');
+    }
+    if (attr.name === 'class') {
+      count++;
+      assert.equal(attr.value, 'cls');
+    }
+    if (attr.name === 'data') {
+      count++;
+      assert.equal(attr.value, 'abc');
+    }
+  }
+
+  assert.ok(count === 3);
+});
+
+it('script', function () {
+  let content = `
+    <div>abcd</div>
+  <span>x</span>
+  /script
+  <script
+  <
+  </
+  </s
+  </sc
+  </scr
+  </scri
+  </scrip
+  </script 
+  `;
+  let document = parseHTML.parseHTML(`<script>${content}</script>`);
+  let text = document.children[0].children[0];
+
+  assert.equal(text.content, content);
+  assert.equal(text.type, 'text');
+});
+
+it('attribute with no value', function () {
+  let document = parseHTML.parseHTML('<div class />');
+  let div = document.children[0];
+
+  let count = 0;
+
+  for (const attr of div.attributes) {
+    if (attr.name === 'class') {
+      count++;
+      assert.equal(attr.value, '');
+    }
+  }
+
+  assert.ok(count === 1);
+});
